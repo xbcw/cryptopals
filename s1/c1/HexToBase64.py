72f83c8d6f2b9aa8ef12a59f7772563887b60c04
Brad Williams <bcw1980@gmail.com>
2015-07-13 00:03:54 -0500

    s1c2

--- s1/c1/HexToBase64.py
+++ s1/c1/HexToBase64.py
@@ -1,16 +1,19 @@
 import sys
+import os
 
 def main(argv):
-	input_string = argv[1]
-	print hex_to_base64(input_string)
+	print hex_to_base64(argv[1])
 
 def hex_to_base64(input_string):
 	try:
 		int(input_string,16)
+		return input_string.decode('hex').encode('base64').strip('\n')
 	except ValueError as e:
-		print "Error: '%s' is not a hexidecimal number." % input_string
+		print "Error in '%s': '%s' is not a hexidecimal number." % (os.path.basename(__file__), input_string)
 		return -1
-	return input_string.decode('hex').encode('base64').strip('\n')
+	except Exception as e:
+		print "Error in '%s': %s" % (os.path.basename(__file__), str(e))
+		return -1
 
 if __name__ == "__main__":
 	main(sys.argv)
